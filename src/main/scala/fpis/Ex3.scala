03971ce4830d34ea50c6ebfeb1c1159621554767
Yevhenii_Dermenzhii <Yevhenii.Dermenzhii.-ND@disney.com>
2026-01-26 23:02:51 -0500

    foldLeft

--- src/main/scala/fpis/Ex3.scala
+++ src/main/scala/fpis/Ex3.scala
@@ -52,12 +52,42 @@ def init[A](as: List[A]): List[A] = {
   }
 }
 
+def foldRight[A, B](as: List[A], acc: B, f: (A, B) => B): B = as match {
+  case List.Nil              => acc
+  case List.Cons(head, tail) => f(head, foldRight(tail, acc, f))
+}
+
+// 3.9
+def length[A](as: List[A]): Int = foldRight(as, 0, (_, acc) => acc + 1)
+
+// 3.10
+@tailrec
+def foldLeft[A, B](as: List[A], acc: B, f: (A, B) => B): B = as match {
+  case List.Nil        => acc
+  case List.Cons(h, t) => foldLeft(t, f(h, acc), f)
+}
+
+// 3.11
+def product(as: List[Int]): Int = foldLeft(as, 1, _ * _)
+def sum(as: List[Int]): Int = foldLeft(as, 0, _ + _)
+
+// 3.12
+def reverse[A](as: List[A]): List[A] = ???
+
 object Test extends App {
-  val l1 = List(1, 2, 3, 4, 5)
+  val l1: List[Int] = List(1, 2, 3, 4, 5)
+
   println(l1)
   println(tail(l1))
   println(setHead(10, l1))
   println(drop(l1, 2))
   println(dropWhile(l1, _ < 3))
   println(init(l1))
+  println(foldRight(l1, 0, _ + _))
+  println(length(l1))
+// fails with a  stack overflow error
+//  println(length(List(1 to 20000*)))
+  println(foldLeft(l1, 1, _ * _))
+  println(product(l1))
+  println(sum(l1))
 }
