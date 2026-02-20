a43a59021b112c57b794e71f3dba8f7fe970b3ec
yevhenii <dzhonu@ukr.net>
2026-02-19 21:38:10 -0500

    side effects - random generator

--- src/main/scala/fpis/Ch6.scala
+++ src/main/scala/fpis/Ch6.scala
@@ -1,5 +1,9 @@
 package fpis
 
+import scala.annotation.tailrec
+import scala.List
+import scala.LazyList
+
 object Ch6 extends App {
 
   trait RNG {
@@ -28,6 +32,38 @@ object Ch6 extends App {
     (n / (Int.MaxValue.toDouble + 1), r)
   }
 
+  // 6.3
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = nonNegativeInt(rng)
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, r1) = double(rng)
+    val (i, r2) = nonNegativeInt(r1)
+    ((d, i), r2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    (d1, d2, d3) -> r3
+  }
+
+  // 6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @tailrec
+    def ints0(cnt: Int, r: RNG, l: scala.List[Int]): (List[Int], RNG) = {
+      if cnt == 0 then (l, r)
+      else
+        val (i, r2) = r.nextInt
+        ints0(cnt - 1, r2, i :: l)
+    }
+    ints0(count, rng, Nil)
+  }
+
   // 6.1
   val r = SimpleRNG(30)
   val l: LazyList[Int] = LazyList.unfold[Int, RNG](r)(rng => Some(nonNegativeInt(rng)))
@@ -37,4 +73,21 @@ object Ch6 extends App {
   val l2 = LazyList.unfold[Double, RNG](r)(rng => Some(double(rng)))
   println(l2.take(10).toList)
 
+  // 6.3
+  println(
+    LazyList
+      .unfold[(Int, Double), RNG](r)(rng => Some(intDouble(rng)))
+      .take(5)
+      .toList
+  )
+  println(LazyList.unfold[(Double, Int), RNG](r)(rng => Some(doubleInt(rng))).take(5).toList)
+  println(
+    LazyList
+      .unfold[(Double, Double, Double), RNG](r)(rng => Some(double3(rng)))
+      .take(5)
+      .toList
+  )
+
+  // 6.4
+  println(ints(5)(r))
 }
